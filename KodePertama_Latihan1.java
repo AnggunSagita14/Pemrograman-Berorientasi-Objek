ef2f132a27d9beaf8c735c22e2125a6b2162c514
AnggunSagita14 <d1041241085@student.untan.ac.id>
2026-02-05 15:01:11 +0700

    Pertemuan1

---
+++ KodePertama_Latihan1.java
@@ -0,0 +1,13 @@
+public class KodePertama_Latihan1{
+    public static void main(String[] args) {
+        System.out.println("Nama        : Anggun Sagita");
+        System.out.println("NIM         : D1041241085");
+        System.out.println("Semester    : 4");
+        System.out.println("Prodi       : Informatika");
+
+    }
+}
+/*
+ NAMA    : ANGGUN SAGITA
+ TANGGAL :  05/02/2026
+ */
