0695c9f5e3f640caf43396969c6db15b036a717e
AnggunSagita14 <d1041241085@student.untan.ac.id>
2026-02-05 15:11:22 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,11 @@
+package Pertemuan1;
+public class KodePertama {
+public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
+/*
+ NAMA    : ANGGUN SAGITA
+ TANGGAL :  05/02/2026
+ */
