5ccec578c41e35786e5e349b538f00527ee43d4e
AnggunSagita14 <d1041241085@student.untan.ac.id>
2026-02-13 08:15:24 +0700

    Pertemuan 3
    Latihan 2 Pertemuan 3

---
+++ Pertemuan3/LatihPerhitungan.java
@@ -0,0 +1,21 @@
+package Pertemuan3;
+
+public class LatihPerhitungan {
+    public static void main(String[] args) {
+        int saldo = 1000000;
+        int setoran = 500000;
+        int penarikan = 250000;
+
+        System.out.println("=== SIMULASI TRANSAKSI  BANK ===");
+        System.out.println("Saldo awal             :" + saldo);
+        saldo  += setoran; // menambahkan setoran ke saldo
+        System.out.println("Setoran                :" + setoran);
+        System.out.println("Saldo Setelah Setor    :" + saldo);
+
+        saldo -= penarikan; // mengurangi saldo dengan penarikan
+        System.out.println("Penarikan              :" + penarikan);
+        System.out.println("Saldo Akhir            :" + saldo);
+
+
+    }
+}
