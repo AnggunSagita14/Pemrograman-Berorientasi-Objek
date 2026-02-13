e5e210acb646dee3a29472f3a12994c4cd3e2aaf
AnggunSagita14 <d1041241085@student.untan.ac.id>
2026-02-13 08:40:00 +0700

    Pertemuan4

---
+++ Pertemuan4/p042OperatorAritmatika.java
@@ -0,0 +1,25 @@
+package Pertemuan4;
+
+public class p042OperatorAritmatika {
+    public static void main(String[] args) {
+int a = 10;
+int b = 3;
+System.out.println("a = " + a);
+System.out.println("b = " + b);
+System.out.println();
+System.out.println("a + b = " + (a + b));
+System.out.println("a - b = " + (a - b));
+System.out.println("a * b = " + (a * b));
+System.out.println("a / b = " + (a / b));
+System.out.println("a % b = " + (a % b));
+// Pembagian dengan desimal
+double hasil = (double) a / b;
+System.out.println("a / b (desimal) = " + hasil);
+    }
+}
+
+/*
+ NAMA    : ANGGUN SAGITA
+ NIM     : D1041241085
+ TANGGAL :  13/02/2026
+ */
