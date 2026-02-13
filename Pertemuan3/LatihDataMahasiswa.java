c6307d012a17caf3d401f1e3c6510b345a857043
AnggunSagita14 <d1041241085@student.untan.ac.id>
2026-02-13 08:13:35 +0700

    Pertemuan3
    Latihan 1 Pertemuan 3

---
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,20 @@
+package Pertemuan3;
+
+public class LatihDataMahasiswa {
+    public static void main(String[] args) {
+        long nim = 2301010001L; // menggunakan long karena angkanya besar
+        String nama = "Budi Santoso"; // string untuk teks/nama
+        char kelas = 'A'; // char untuk satu karakter
+        boolean statusAktif = true; // boolean untuk (true/false)
+        double ipkSemester = 3.75; // double untuk angka desimal
+        int totalSks = 72; //int untuk blgn bulat
+
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM            : " + nim);
+        System.out.println("Nama           : " + nama);
+        System.out.println("Kelas          : " + kelas);
+        System.out.println("Status Aktif   : " + statusAktif);
+        System.out.println("IPK Semester   : " + ipkSemester);
+        System.out.println("Total SKS      : " + totalSks);
+    }
+}
